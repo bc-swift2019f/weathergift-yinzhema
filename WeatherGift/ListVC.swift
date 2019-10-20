488aaeab92508d0d789542ee6a0ba8df3bb54e97
yinzhema <yma9826@gmail.com>
2019-10-20 17:29:30 -0400

    session 7.9 complete

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 class ListVC: UIViewController {
     
@@ -46,6 +47,12 @@ class ListVC: UIViewController {
         }
     }
     
+    
+    @IBAction func addBarButtonPressed(_ sender: UIBarButtonItem) {
+        let autocompleteController=GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController,animated:true,completion:nil)
+    }
 
 }
 
@@ -86,4 +93,41 @@ extension ListVC: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
         return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath:proposedDestinationIndexPath)
     }
+    
+    func updaterTable(place: GMSPlace){
+        let newIndexPath=IndexPath(row: locationsArray.count, section: 0)
+        locationsArray.append(place.name!)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+    }
+    
+}
+
+extension ListVC: GMSAutocompleteViewControllerDelegate {
+    
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        dismiss(animated: true, completion: nil)
+        updaterTable(place: place)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
 }
